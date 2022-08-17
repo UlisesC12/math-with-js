23159d1de5cb9d05c457ebadf8a8ece2468c361d
UlisesC12 <39888086+UlisesC12@users.noreply.github.com>
2022-08-17 01:41:12 -0500

    add discount codes
    simulate API Request with an array of coupon codes.

--- descuentos.js
+++ descuentos.js
@@ -3,15 +3,31 @@ const inputDiscount = document.querySelector('#discount');
 const btnDiscount = document.querySelector('#calcular');
 const resultTxt = document.querySelector('#result');
 
+let couponCodes = {
+    'PLATZI90': 90,
+    'PLATZI80': 80,
+    'PLATZI70': 70,
+    'PLATZI60': 60,
+    'PLATZI50': 50,
+    'PLATZI40': 40,
+    'PLATZI30': 30,
+    'PLATZI20': 20,
+    'PLATZI10': 10,
+    'PLATZI05': 5,
+}
 
 btnDiscount.addEventListener('click', calcularTotal);
 
 function calcularTotal(){
     const price = Number(inputPrice.value);
-    const discount = Number(inputDiscount.value);
-
-    if(!price || !discount) { 
-        resultTxt.innerHTML = "Por favor, llena el formulario";
+    const discount = couponCodes[inputDiscount.value];
+    console.log(inputDiscount.value);
+    if(!price) { 
+        resultTxt.innerHTML = "Por favor, especifique el precio.";
+        return;
+    };
+    if(!discount) { 
+        resultTxt.innerHTML = "ERROR: No se encontró código válido";
         return;
     };
     if(discount >= 100){
@@ -19,5 +35,5 @@ function calcularTotal(){
         return;
     }
     let total = (price * (100 - discount)) / 100;
-    resultTxt.innerHTML = `El precio con descuento es: $${total}`;
+    resultTxt.innerHTML = `El Descuento es de: ${discount}%<br>El precio con descuento es: $${total}`;
 }
